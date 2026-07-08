40db32eaddbecc72705cecee85ac2cc126307fc4
Hasan <hasanj2404@outlook.com>
2026-07-09 04:57:15 +0530

    feat: basic traditional index.html

---
+++ README.md
@@ -0,0 +1,5 @@
+# Full stack application tutorial
+
+The main focus of this tutorial is to learn Spring boot and make it work with
+frontend technology like React.js.
+The primary database used will be ``postgres``.
---
+++ store/src/main/java/com/hasanjamil/store/StoreController.java
@@ -0,0 +1,13 @@
+package com.hasanjamil.store;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class StoreController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index.html";
+    }
+}
