eac8a0eac1e3bf930fe79aa2b519f6b430333eda
Hasan <hasanj2404@outlook.com>
2026-07-13 03:15:58 +0530

    fix(user-service): added missing annotation

--- user-service/src/main/java/com/example/user/service/UserService.java
+++ user-service/src/main/java/com/example/user/service/UserService.java
@@ -3,6 +3,9 @@ package com.example.user.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
     public List<String> getUsers() {
         ArrayList<String> users = new ArrayList<String>();
